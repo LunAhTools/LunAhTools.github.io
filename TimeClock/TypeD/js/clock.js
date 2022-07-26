67472a38be88a76ffdf3df6ca969ca0880db0592
LunAhEric <luochen9913@gmail.com>
2022-07-26 17:31:33 +0800

    修正TypeD 動畫樣式 修正錯字 移除無用程式碼

--- TimeClock/TypeD/js/clock.js
+++ TimeClock/TypeD/js/clock.js
@@ -3,45 +3,12 @@ const app = new Vue({
   data: {
     time: moment(),
     timer: null,
-    sec: null,
-    cirtimer: null,
-    styleClass: {
-      R: 0,
-      G: 0,
-      B: 0,
-    },
-    deg: 0,
-    degrever:0,
   },
   created() {
     this.momentSetting()
     this.timer = setInterval(() => {
       this.time = moment()
-      this.getTimeSec()
-      this.styleClass.R = this.getRandom(1, 255)
-      this.styleClass.G = this.getRandom(1, 255)
-      this.styleClass.B = this.getRandom(1, 255)
     }, 500)
-    this.cirtimer = setInterval(() => {
-      if (this.degrever == 360) {
-        this.degrever = 0
-      }
-      this.degrever += 10
-    }, 24);
-  },
-  computed: {
-    clockstyle() {
-      return {
-        'transition': '1s',
-        'color': 'rgb(' + this.styleClass.R + ',' + this.styleClass.G + ',' + this.styleClass.B + ')'
-      }
-    },
-    cirsec() {
-      return {
-        // 'transition': '1s',
-        'transform': 'rotate(-' + this.degrever + 'deg)'
-      }
-    }
   },
   methods: {
     momentSetting() {
@@ -51,10 +18,6 @@ const app = new Vue({
     },
     getRandom(min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    getTimeSec() {
-      this.sec = this.time.utc().seconds()
     }
   }
 })
-
